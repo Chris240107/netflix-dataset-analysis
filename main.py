f26776dc854b561b08ccbaa2bfd5b692c9e2f498
unknown <chrisyadavneedshelp@gmail.com>
2026-05-13 16:24:47 +0530

    Replacing null values

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import pandas as pd
+import numpy as np
 
-data = pd.read_csv('netflix_titles.csv') #giving pandas access to the csv
+df = pd.read_csv('netflix_titles.csv') #giving pandas access to the csv
 
 #understanding the dataset
-print(data.head())
-print(data.info())
-missing_values = data.isnull().sum()
+print(df.head())
+print(df.info())
+missing_values = df.isnull().sum()
 
-total_cells = data.size
+print(missing_values)
+
+total_cells = df.size
 total_missing = missing_values.sum()
 
-percent_missing = (total_missing / total_cells) * 100
+percent_missing = (total_missing / total_cells) * 100 # around 4% of the data is missing.
 
 print(percent_missing)
+
+# fill missing values to have some sort of semblance
+
+#df['country'] = df['country'].fillna(df['country'].mode()[0])
+#df['cast'].replace(np.nan, 'No Data', inplace = True)
+
+df.fillna({'director' : 'No Data'}, inplace = True)
+print(df.loc[:, ['director']])
+
+check = df[df['director'].isnull()]
+print(check)
+
