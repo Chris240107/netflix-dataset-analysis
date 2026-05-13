24f174ad66ea6ee49c379f8d65fdf853c5e431bf
unknown <chrisyadavneedshelp@gmail.com>
2026-05-13 13:42:17 +0530

    first steps to clean data

--- main.py
+++ main.py
@@ -5,5 +5,11 @@ data = pd.read_csv('netflix_titles.csv') #giving pandas access to the csv
 #understanding the dataset
 print(data.head())
 print(data.info())
-print(data.isnull().sum())
+missing_values = data.isnull().sum()
 
+total_cells = data.size
+total_missing = missing_values.sum()
+
+percent_missing = (total_missing / total_cells) * 100
+
+print(percent_missing)
